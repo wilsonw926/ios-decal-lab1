a2e566cc4575a72a8e44664e16a9ada070330037
Akilesh Bapu <akilesh@berkeley.edu>
2017-01-30 17:03:55 -0800

    Exercise declare variables done

---
+++ XcodeTour/XcodeTour/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  XcodeTour
+//
+//  Created by Akilesh Bapu on 1/30/17.
+//  Copyright © 2017 org.iosdecal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var textToDisplay: [String] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        print("hello world")
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        getTextToDisplay()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func matchTheArrayHere() {
+        performSegue(withIdentifier: "toNice", sender: nil)
+    }
+    
+    func getTextToDisplay() {
+        let data1 = [1701013838, 1919899424, 1847602283, 1830844271, 543520367, 1948282479, 1752440943, 1701716069, 1931506808, 1701273972]
+        let data2 = Data(buffer: UnsafeBufferPointer(start: data1, count: data1.count))
+        let textToDisplay = NSString(data: data2, encoding: String.Encoding.utf8.rawValue)
+        let formattedText = textToDisplay?.capitalized
+        let formattedTextArray = [formattedText]
+        
+        //YOUR CODE HERE
+        self.textToDisplay = formattedTextArray as! [String]
+        
+        //END YOUR CODE
+        self.matchTheArrayHere()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destinationVC = segue.destination as? NiceWorkViewController {
+            //YOUR CODE HERE
+            //Get textToDisplay to equal formattedTextArray
+            destinationVC.textToDisplay = self.textToDisplay
+            
+            //END YOUR CODE
+        }
+    }
+    func print(_ items: Any?) {
+        //You are not allowed to use print to debug!
+        print("DON'T USE PRINT STATEMENT")
+    }
+}
+
